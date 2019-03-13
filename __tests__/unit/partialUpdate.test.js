a617fc03a6e6b1d1e6cfea5584c1d0590624202b
Gabriela <balicas.gb@gmail.com>
2019-03-13 10:34:29 -0700

    create test for partialUpdate function

--- __tests__/unit/partialUpdate.test.js
+++ __tests__/unit/partialUpdate.test.js
@@ -1,9 +1,24 @@
+const sqlForPartialUpdate = require('../../helpers/partialUpdate')
+
+const items = {
+  'column1': 'value1',
+  '_column2': 'value2', 
+  'column3': 'value3'
+}
+
+
 describe("partialUpdate()", () => {
   it("should generate a proper partial update query with just 1 field",
       function () {
+        const response = sqlForPartialUpdate('tests', items, 'testA', 'testB')
 
-    // FIXME: write real tests!
-    expect(false).toEqual(true);
+        // FIXME: write real tests!
+        expect(typeof response).toEqual('object');
+        expect(typeof response.query).toEqual('string');
+        expect(response.query).toEqual('UPDATE tests SET column1=$1, column3=$2 WHERE testA=$3 RETURNING *');
+        expect(Array.isArray(response.values)).toEqual(true);
+        expect(response.values).toEqual(["value1", "value3", "testB"]);
 
   });
 });
+
