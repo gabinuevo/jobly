b844bbaa45c074ffe19618d8376c1edb55e0e03f
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-13 11:16:27 -0700

    Add getAll companies to company Model.

--- models/companies.js
+++ models/companies.js
@@ -9,17 +9,18 @@ class Company {
      * [{handle, name}, ...]
      */
 
-     static async getAll({ search='', min_employess=0, max_employees=Infinity }) {
+     static async getAll({ search='%', min_employess=0, max_employees=Infinity }) {
         const result = await db.query(
             `SELECT
              handle,
              name,
              FROM companies
              WHERE num_employees>$1 AND num_employees<$2
-             AND name OR $3
-             LIKE `,
+             AND name
+             LIKE '$3' `,
              [min_employess, max_employees, search]
-        )
+        );
+        return result.rows;
      } 
 
-}ˀ
+}
