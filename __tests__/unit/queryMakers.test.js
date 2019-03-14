72a21fb6fee20ffb3b7c3945083928a87415bea3
Stéphane Dryefus <floatingrock@gmail.com>
2019-03-14 11:50:18 -0700

    Add makeGetQuery tests.

--- __tests__/unit/queryMakers.test.js
+++ __tests__/unit/queryMakers.test.js
@@ -8,7 +8,6 @@ const items = {
   'logo_url': 'www.testing.com'
 }
 
-// Needs much modification!
 describe("makeInsertQuery()", () => {
   it("generates variable INSERT query string depending on user input",
       function () {
@@ -22,5 +21,22 @@ describe("makeInsertQuery()", () => {
   });
 });
 
+let searchItems = {
+  'search': 'value1',
+  'min_employees': 'value2', 
+  'max_employees': 'value3'
+}
 
-     
+describe("makeGetQuery()", () => {
+  it("generates variable SELECT query string depending on user input",
+      function () {
+        const response = makeGetQuery(searchItems);
+        
+        expect(typeof response).toEqual('object');
+        expect(typeof response.query).toEqual('string');
+        expect(response.query).toEqual('SELECT handle, name FROM companies WHERE num_employees>$1 AND num_employees<$2 AND name ILIKE $3 OR name ILIKE $4 OR name ILIKE $5');
+        expect(Array.isArray(response.searchParams)).toEqual(true);
+        expect(response.searchParams).toEqual(["value2", "value3", "value1%", "%value1%", "%value1"]);
+
+  });
+});  
